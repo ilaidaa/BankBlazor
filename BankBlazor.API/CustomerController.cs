e42cbc5b7dcd4149384ba66e7348ea4610305986
ilaidaa <ilaida.e@hotmail.com>
2025-04-01 21:05:25 +0200

    Skapar en controller som är en klass som har hand om allt som rör kunder.

---
+++ BankBlazor.API/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BankBlazor.API.Models;
+
+namespace BankBlazor.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly BankContext _context;
+
+        public CustomerController(BankContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/customer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET: api/customer/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(c => c.CustomerId == id);
+
+            if (customer == null)
+                return NotFound();
+
+            return customer;
+        }
+    }
+}
