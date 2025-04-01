2d05777f555c2cccf98dc2572447d9298a7b936d
ilaidaa <ilaida.e@hotmail.com>
2025-04-01 21:33:28 +0200

    Jag la till BankContext i Program och Json.  API:t vet hur det ska prata med databasen nu

--- BankBlazor.API/Program.cs
+++ BankBlazor.API/Program.cs
@@ -1,4 +1,5 @@
-
+using Microsoft.EntityFrameworkCore;
+using BankBlazor.API.Models; 
 namespace BankBlazor.API
 {
     public class Program
@@ -7,16 +8,19 @@ namespace BankBlazor.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Denna rad är till för customerController
+            builder.Services.AddDbContext<BankContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+           
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+           
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
