209b22abf7bbf07283253d8ff8a619d0f56a22a6
ilaidaa <ilaida.e@hotmail.com>
2025-04-02 16:07:12 +0200

    Lagt till GET funktion så att AccountController kan ta emot Account ID och även ta fram transaktioner kopplat till det konto ID:t

--- BankBlazor.API/Controllers/AccountController.cs
+++ BankBlazor.API/Controllers/AccountController.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BankBlazor.API.Models;
 
 namespace BankBlazor.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    // Controller som ska hantera konto och pengar
+
+    [Route("api/[controller]")]             //Vart jag kan hitta den
+    [ApiController]                         // Säger : "Detta är en API Controller
     public class AccountController : ControllerBase
     {
+        private readonly BankContext _context; // Privat så bara klassen ska använda
+                                               // readonly menar att den bara kan sättas en gång
+                                               // BankContext är min koppling till databasen
+                                               // _context Variabeln som används i resten av klassen för att komma åt databasen
+
+        public AccountController(BankContext context) // Den här konstruktorn är hjärtat i hur controller får tillgång till databasen.
+        {
+            _context = context;
+        }
+
+        // GET: api/Account/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Account>> GetAccount(int id) // GetAccount namnet på metoden som ska ta emot id
+                                                                    // sjäva metoden är allts som finns inom klammerparenteserna nedan
+        {
+            var account = await _context.Accounts
+                .Include(a => a.Transactions)
+                .FirstOrDefaultAsync(a => a.AccountId == id);
+            //    await: Väntar på att databasen ska svara (utan att låsa systemet under tiden)
+            //   _context.Accounts: Går till tabellen / entiteten Accounts i databasen.
+            //   .Include(a => a.Transactions): Hämtar även alla transaktioner kopplade till kontot. (Så man får med både kontot och dess transaktioner.)
+            //   .FirstOrDefaultAsync(...): Hämtar det första kontot som har det här id, eller null om inget hittas.
+
+            if (account == null)
+            {
+                return NotFound(); // 404
+            }
+                
+
+            return account;
+        }
     }
 }
