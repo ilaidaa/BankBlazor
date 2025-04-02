f06168bae95354555589c63c6d1dc9b362f1aa4a
ilaidaa <ilaida.e@hotmail.com>
2025-04-02 15:16:22 +0200

    Lagt in några kommentarer så jag minns exakt vad som händer.

--- BankBlazor.API/Controllers/CustomerController.cs
+++ BankBlazor.API/Controllers/CustomerController.cs
@@ -4,6 +4,8 @@ using BankBlazor.API.Models;
 
 namespace BankBlazor.API.Controllers
 {
+    // Controller som sköter Kunder
+
     [ApiController]
     [Route("api/[controller]")]
     public class CustomerController : ControllerBase
@@ -15,23 +17,28 @@ namespace BankBlazor.API.Controllers
             _context = context;
         }
 
-        // GET: api/customer
+        // GET: api/customer Ska hämta alla kunder
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers() // Hämtar listan av kunder
         {
-            return await _context.Customers.ToListAsync();
+            return await _context.Customers.ToListAsync(); // returnerar listan av kunder
         }
 
-        // GET: api/customer/1
+        // GET: api/customer/1 ska hämta kund efter ID
         [HttpGet("{id}")]
         public async Task<ActionResult<Customer>> GetCustomer(int id)
         {
+            // Kolla i Databasen första bästa som matchar
             var customer = await _context.Customers
                 .FirstOrDefaultAsync(c => c.CustomerId == id);
 
+            // Hantera om det inte finns
             if (customer == null)
-                return NotFound();
-
+            {
+                return NotFound(); //  404 Not Found.
+            }
+              
+            // returnera customer om det finns
             return customer;
         }
     }
