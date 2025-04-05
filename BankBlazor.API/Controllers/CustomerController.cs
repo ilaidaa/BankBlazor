06b40cc34f9491373ab4e9cd165587e96abb4b8c
ilaidaa <ilaida.e@hotmail.com>
2025-04-05 15:02:37 +0200

    La till små kommentarer för att komma ihåg exakt vad som händer

--- BankBlazor.API/Controllers/CustomerController.cs
+++ BankBlazor.API/Controllers/CustomerController.cs
@@ -10,9 +10,13 @@ namespace BankBlazor.API.Controllers
     [Route("api/[controller]")]
     public class CustomerController : ControllerBase
     {
-        private readonly BankContext _context;
+     
+        private readonly BankContext _context; // Privat så bara klassen ska använda
+                                               // readonly menar att den bara kan sättas en gång
+                                               // BankContext är min koppling till databasen
+                                               // _context Variabeln som används i resten av klassen för att komma åt databasen
 
-        public CustomerController(BankContext context)
+        public CustomerController(BankContext context) // Den här konstruktorn är hjärtat i hur controller får tillgång till databasen.
         {
             _context = context;
         }
@@ -26,11 +30,15 @@ namespace BankBlazor.API.Controllers
 
         // GET: api/customer/1 ska hämta kund efter ID
         [HttpGet("{id}")]
-        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        public async Task<ActionResult<Customer>> GetCustomer(int id)  // Meyoden getCustomer skapas den tar emot id
+                                                                       // sjäva metoden är allts som finns inom klammerparenteserna nedan
         {
             // Kolla i Databasen första bästa som matchar
             var customer = await _context.Customers
                 .FirstOrDefaultAsync(c => c.CustomerId == id);
+            //    await: Väntar på att databasen ska svara (utan att låsa systemet under tiden)
+            //   _context.Accounts: Går till tabellen / entiteten Accounts i databasen.
+            //   .FirstOrDefaultAsync(...): Hämtar det första kunden som har det här id, eller null om inget hittas.
 
             // Hantera om det inte finns
             if (customer == null)
