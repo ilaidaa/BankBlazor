30210f4954b91b8925639accdb1dfb6af0e72b6d
ilaidaa <ilaida.e@hotmail.com>
2025-04-05 15:07:33 +0200

    Skapat en ny controller som ska sköta insättning av pengar. Döpt den till TransactionController

---
+++ BankBlazor.API/Controllers/TransactionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankBlazor.API.Controllers
+{
+    [Route("api/[controller]")] // Själva adressen vart jag ska hitta dvs endpoint
+    [ApiController] // Visar att det är en controller
+    public class TransactionController : ControllerBase  // ärver från ControllerBase
+    {
+    }
+}
